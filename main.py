cb3d689d78923dab450db7f1fd15603334867f55
DapperPixelguy <32307494+DapperPixelguy@users.noreply.github.com>
2024-02-22 11:20:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ class ShortcutManagerApp():
     self.root.geometry("200x400")
     self.title = tk.Label(text='Shortcut Manager')
     self.title.pack()
-    
+
     self.con = sq.connect('locations.db')
     self.cur = self.con.cursor()
-    
+
     self.shortFrame = tk.Frame(root)
     self.init_shortcuts(self.shortFrame)
-    
+
     self.createBtn = tk.Button(text='Create Shortcut', command=lambda: self.change_packing('create'))
     self.createBtn.pack()
-    
+
     self.deleteBtn = tk.Button(text='Delete Shortcut', command=lambda: self.change_packing('delete'))
     self.deleteBtn.pack()
-    
+
     self.nameField = tk.Entry()
     self.confirmBtn = tk.Button(text='Confirm')
-  
+
   def init_shortcuts(self, frame):
       self.cur.execute('''
       CREATE TABLE IF NOT EXISTS users (
@@ -39,10 +39,10 @@ class ShortcutManagerApp():
       rows = self.cur.fetchall()
       print(rows)
       print(len(rows))
-  
+
       for widget in frame.winfo_children():
         widget.destroy()
-  
+
       self.cur.execute('SELECT * FROM users')  
       for row in self.cur:
         name = row[0]
@@ -51,22 +51,21 @@ class ShortcutManagerApp():
         newBtn = tk.Button(frame, text=name, command=lambda path=file : os.startfile(path))
         newBtn.pack()
       frame.pack()
-  
-  
+
+
   def change_packing(self, arg):
     if arg == 'create':
-      #[entry.pack_forget() for entry in [forget list]] <-- DO THIS
-      self.createBtn.pack_forget()
-      self.shortFrame.pack_forget()
-      self.deleteBtn.pack_forget()
+      [entry.pack_forget() for entry in [self.createBtn, self.shortFrame, self.deleteBtn]]
       self.title.config(text=f'Creating shortcut')
       self.nameField.pack()
+      
       self.confirmBtn.config(command=lambda: self.create_shortcut())
       self.confirmBtn.pack()
     if arg == 'delete':
       self.init_delete()
       self.createBtn.pack_forget()
       self.deleteBtn.pack_forget()
+      
       self.title.config(text=f'Deleting shortcut')
       self.confirmBtn.config(command=lambda: self.delete_shortcut())
       self.confirmBtn.pack()
@@ -74,13 +73,15 @@ class ShortcutManagerApp():
       self.init_shortcuts(self.shortFrame)
       self.createBtn.pack()
       self.deleteBtn.pack()
+      
       self.title.config(text=f'Shortcut Manager')
       self.nameField.delete(0,tk.END)
       self.nameField.pack_forget()
+      
       self.confirmBtn.config(command=None)
       self.confirmBtn.pack_forget()
-  
-  
+
+
   def create_shortcut(self):
     global nameField, con, cur
     name = self.nameField.get()
@@ -94,13 +95,13 @@ class ShortcutManagerApp():
         self.con.execute('INSERT INTO users (name, path) VALUES (?,?)', (name, file))
         self.con.commit()
         self.change_packing('home')
-  
+
   def init_delete(self):
       global shortFrame
       for entry in self.shortFrame.winfo_children():
           entry.configure(command=lambda text=entry.cget('text'): self.shortcut_delete(text))
-  
-  
+
+
   def shortcut_delete(self, name):
       global shortFrame
       self.cur.execute('DELETE FROM users WHERE name = ?', (name,))
@@ -108,8 +109,8 @@ class ShortcutManagerApp():
       self.init_delete()
       print('Ran')
       self.con.commit()
-  
-  
+
+
   def delete_shortcut(self):
       self.cur.execute('SELECT * FROM users')
       rows = self.cur.fetchall()
